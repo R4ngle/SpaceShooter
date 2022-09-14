5a65b63d418d5834eebf5bed7e75ee9da31fe50e
R4ngle <patrickmcglaughlin900@gmail.com>
2022-09-14 14:17:03 -0400

    coding today 9/17/22
    finishing bullet script

--- SpaceShooterPro/SpaceShooterPro/Assets/Scripts/Player.cs
+++ SpaceShooterPro/SpaceShooterPro/Assets/Scripts/Player.cs
@@ -4,52 +4,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
-    // Start is called before the first frame update
+  
+    [SerializeField]
+    private float fireRate = .3f;
+    private float nextFire = -1;
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
-       CalculateMovement();
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
-
-        }
-       
-      
+        CalculateMovement();
+        FireLaser();  
     }
-void CalculateMovement()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticaInput = Input.GetAxis("Vertical");
+          void CalculateMovement() { 
+        {
 
-        Vector3 direction = new Vector3(horizontalInput, verticaInput, 0);
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(direction * _speed * Time.deltaTime);
+            Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, 3.8f, 0);
-        }
-        if (transform.position.x > 11.3f)
-        {
-            transform.position = new Vector3(-11.3f, transform.position.y, 0);
-        }
-        else if (transform.position.x < -11.3f)
-        {
-            transform.position = new Vector3(11.3f, transform.position.y, 0);
+            transform.Translate(direction * _speed * Time.deltaTime);
+
+            if (transform.position.y >= 0)
+            {
+                transform.position = new Vector3(transform.position.x, 0, 0);
+            }
+            else if (transform.position.y <= -3.8f)
+            {
+                transform.position = new Vector3(transform.position.x, 3.8f, 0);
+            }
+            if (transform.position.x > 11.3f)
+            {
+                transform.position = new Vector3(-11.3f, transform.position.y, 0);
+            }
+            else if (transform.position.x < -11.3f)
+            {
+                transform.position = new Vector3(11.3f, transform.position.y, 0);
+            }
+     void FireLaser() {
+
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Instantiate(_laserPrefab, GameObject.FindWithTag("Player").transform.position + new Vector3(0, .7f, 0), Quaternion.identity);
+
+            }
         }
     }
 }
