a77aea09b3e056e22c03fcb291109e183c97911c
R4ngle <111386276+R4ngle@users.noreply.github.com>
2022-09-22 15:31:19 -0400

    bug fixes

--- SpaceShooterPro/SpaceShooterPro/Assets/Scripts/Player.cs
+++ SpaceShooterPro/SpaceShooterPro/Assets/Scripts/Player.cs
@@ -8,56 +8,63 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
-  
+
     [SerializeField]
     private float fireRate = .3f;
     private float nextFire = -1;
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
-       
+
     }
 
     void Update()
     {
         CalculateMovement();
-        FireLaser();  
-    }
-          void CalculateMovement() { 
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
 
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
+            nextFire = Time.time + fireRate;
+            Instantiate(_laserPrefab, GameObject.FindWithTag("Player").transform.position + new Vector3(0, .7f, 0), Quaternion.identity);
+        }
 
-            Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-            transform.Translate(direction * _speed * Time.deltaTime);
+        void CalculateMovement()
+        {
+            {
 
-            if (transform.position.y >= 0)
-            {
-                transform.position = new Vector3(transform.position.x, 0, 0);
-            }
-            else if (transform.position.y <= -3.8f)
-            {
-                transform.position = new Vector3(transform.position.x, 3.8f, 0);
-            }
-            if (transform.position.x > 11.3f)
-            {
-                transform.position = new Vector3(-11.3f, transform.position.y, 0);
-            }
-            else if (transform.position.x < -11.3f)
-            {
-                transform.position = new Vector3(11.3f, transform.position.y, 0);
-            }
-     void FireLaser() {
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
 
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
-            {
-                nextFire = Time.time + fireRate;
-                Instantiate(_laserPrefab, GameObject.FindWithTag("Player").transform.position + new Vector3(0, .7f, 0), Quaternion.identity);
+                Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
+                transform.Translate(direction * _speed * Time.deltaTime);
+
+                if (transform.position.y >= 0)
+                {
+                    transform.position = new Vector3(transform.position.x, 0, 0);
+                }
+                else if (transform.position.y <= -3.8f)
+                {
+                    transform.position = new Vector3(transform.position.x, 3.8f, 0);
+                }
+                if (transform.position.x > 11.3f)
+                {
+                    transform.position = new Vector3(-11.3f, transform.position.y, 0);
+                }
+                else if (transform.position.x < -11.3f)
+                {
+                    transform.position = new Vector3(11.3f, transform.position.y, 0);
+
+                }
             }
+
+
+
+
+
+
         }
     }
 }
